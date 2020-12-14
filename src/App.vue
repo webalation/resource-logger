eca64b2c84fbc9da8197c92f224bcda6fea143aa
Richard Ball <webalation@gmail.com>
2020-12-14 08:24:59 -0500

    Setup Header and BaseCard components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <the-header title="🗃 Resource Logger"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
-    StoredResources
+    StoredResources,
+    TheHeader
   },
   data() {
     return {
