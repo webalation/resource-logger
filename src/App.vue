62807bb5cdc9ed887b2813046c5367a776a24f59
Richard Ball <webalation@gmail.com>
2020-12-15 07:39:43 -0500

    Dynamic components and attribute fallthrough

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <the-header title="🗃 Resource Logger"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
-    StoredResources,
     TheHeader
   },
   data() {
